120f0bb22911b448b290e651cbc647261c70d341
Sumit A. Adkhale <69811429+sumit-adkhale@users.noreply.github.com>
2025-06-22 23:09:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, HashRouter } from "react-router";
 import AllRouters from "./Components/AllRouters.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/equinox-test">
+    <HashRouter>
       <AllRouters/>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
